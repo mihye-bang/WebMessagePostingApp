e903b31fa8851534be0febe433c00910bc56a934
Mihye <beasjnml@nate.com>
2023-05-02 17:41:34 -0400

    catch the error to make a table name already existing

--- db.py
+++ db.py
@@ -1,12 +1,25 @@
 import sqlite3
 conn = sqlite3.connect('tweets.db')     # creates a tweets.db file
 
-c = conn.cursor()   # assume it's as a handle to run a SQLite commands. allows executing
+# assume it's as a handle to run a SQLite commands. allows executing
+cursor = conn.cursor()
 
-c.execute('''CREATE TABLE tweets (
-    _id integer primary key autoincrement,
-    tweet text,
-    username text
-)''')
+# 'id' is a keyword and so put _ to avoid conflicts
+# cursor.execute('''CREATE TABLE if not exists tweets (
+#         _id integer primary key autoincrement,
+#         tweet text,
+#         username text
+#     )''')
 
-conn.commit()   # don't forget this
+
+def init():
+    try:
+        cursor.execute('''CREATE TABLE tweets (
+            _id integer primary key autoincrement,
+            tweet text,
+            username text
+        )''')
+    except sqlite3.OperationalError as e:
+        print('Table tweet alrady exists. Skipping creation.')
+
+    conn.commit()   # don't forget this - saying I'm done with executing, so it saves the database
